d7fa7e0e28f7ba58ab29fff0771c4c5dd7f194dc
Gek <gek33cr@gmail.com>
2022-09-21 18:31:30 +0300

    самый крутой слайдер что вы видели

---
+++ cool-slider/app.js
@@ -0,0 +1,35 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length - 1;
+const container = document.querySelector('.container');
+let activeSlideIndex = slidesCount; 
+sidebar.style.top = `-${(activeSlideIndex)*100}vh`;
+
+upBtn.addEventListener('click', (e) => {
+	changeSlade('up')
+})
+downBtn.addEventListener('click', (e) => {
+	changeSlade('down')
+})
+function changeSlade(dir){
+	if (dir ==="up") {
+		activeSlideIndex++;
+		if (activeSlideIndex > slidesCount) {
+			activeSlideIndex=0;
+			console.log(activeSlideIndex);
+		}
+	}
+	if (dir ==="down") {
+		activeSlideIndex--;
+		if (activeSlideIndex < 0) {
+			activeSlideIndex=slidesCount;
+			console.log(activeSlideIndex);
+
+		}
+	}
+	const height = container.clientHeight;
+	mainSlide.style.transform = `translateY(-${height*activeSlideIndex}px)`;
+	sidebar.style.transform = `translateY(${height*activeSlideIndex}px)`
+}
